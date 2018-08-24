ea953efe965ebfe05ba1286f3308f6820ee073d3
Bautista luis <luis.bautista@cceo.com.mx>
2018-08-24 11:20:00 -0500

    link a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" type="text/css" href="/libs/estilo.css">
+        <link rel="stylesheet" type="text/css" href="/lib/estilo.css">
 
         <!-- Styles -->
         <style>
